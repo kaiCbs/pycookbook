92a84737d9c0e377a9e0318e1a272c4a4a2c06e5
kai <kxiang21@gsb.columbia.edu>
2020-04-08 19:47:37 -0400

    add 8.3

---
+++ Chapter8/8.3.making_objects_support_the_context_management_protocol.py
@@ -0,0 +1,32 @@
+from functools import partial
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+class LazyConnection:
+    def __init__(self, address, family=AF_INET, type=SOCK_STREAM):
+        self.address = address
+        self.family = AF_INET
+        self.type = SOCK_STREAM
+        self.sock = None
+
+    def __enter__(self):
+        print("enter")
+        if self.sock:
+            raise RuntimeError("Already connected")
+        self.sock = socket(self.family, self.type)
+        self.sock.connect(self.address)
+        return self.sock
+
+    def __exit__(self, exc_ty, exc_val, tb):
+        self.sock.close()
+        self.sock = None
+        print("exit")
+
+
+if __name__ == "__main__":
+    conn = LazyConnection(("www.python.org", 80))
+    with conn as s:
+        s.send(b"GET /index.html HTTP/1.0\r\n")
+        s.send(b"Host: www.python.org\r\n")
+        s.send(b"\r\n")
+        resp = b"".join(iter(partial(s.recv, 8192), b""))
